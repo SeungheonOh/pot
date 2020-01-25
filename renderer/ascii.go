a863745d6437bf5727bfe15e35373195456e30cb
Seungheon Oh <dan.oh0721@gmail.com>
2020-01-24 19:11:55 -0600

    Simple Ascii Renderer

---
+++ renderer/ascii.go
@@ -0,0 +1,49 @@
+package renderer
+
+import (
+	"fmt"
+	"os"
+
+	cv "gocv.io/x/gocv"
+)
+
+/*
+ Simple Brightness based Ascii renderer.
+ No facny pixel masking or stuff
+*/
+
+var Bitmask = [...]rune{
+	' ',
+	' ',
+	'/',
+	'^',
+	'*',
+	'$',
+	'@',
+	'#',
+}
+
+func init() {
+	RendererMap["ascii"] = ascii
+}
+
+func ascii(imgOri cv.Mat) error {
+
+	img := imgOri.Clone()
+	cv.CvtColor(img, &img, 7)
+
+	imgPtr := img.DataPtrUint8()
+
+	fmt.Print("\033[0;0H")
+
+	for i := 0; i < img.Rows(); i += 2 {
+		for j := 0; j < img.Cols(); j++ {
+			fmt.Fprintf(os.Stdout, string(Bitmask[(imgPtr[i*img.Cols()+j]>>5)]))
+		}
+		fmt.Print("\n")
+	}
+	fmt.Print("\n")
+
+	fmt.Print("\033[J")
+	return nil
+}
