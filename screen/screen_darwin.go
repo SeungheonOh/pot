0ec3b94b9941dff974f44979b2b408b08c771a9f
Seungheon Oh <dan.oh0721@gmail.com>
2020-05-17 17:49:10 -0500

    Macos screencapture fix

--- screen/screen_darwin.go
+++ screen/screen_darwin.go
@@ -1,5 +1,9 @@
 package screen
 
+import (
+	"errors"
+)
+
 /*
 This have not been implemented yet
 */
@@ -7,14 +11,14 @@ This have not been implemented yet
 type ScreenGraber struct {
 }
 
-func NewScreenGraber() *XScreenGraber {
-	return &Dummy{}
+func NewScreenGraber() *ScreenGraber {
+	return &ScreenGraber{}
 }
 
 func (X *ScreenGraber) Grab(x, y, w, h int) (ScreenCapture, error) {
-	return nil, errors.New("Your OS does now support screencapturing")
+	return nil, errors.New("Mac OS does now support screencapturing")
 }
 
 func (X *ScreenGraber) GrabById(WinId uint64) (ScreenCapture, error) {
-	return nil, errors.New("Your OS does now support screencapturing")
+	return nil, errors.New("Mac OS does now support screencapturing")
 }
