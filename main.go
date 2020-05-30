b56d6b810a1552f356e75a3be6c99a68c871ea75
Seungheon Oh <dan.oh0721@gmail.com>
2020-05-29 20:18:07 -0500

    comments

--- main.go
+++ main.go
@@ -22,9 +22,12 @@ const (
 	DEFAULT_LOADER   = "FFMPEG"
 )
 
+/*
+	This struct stores all informations and modules that are required.
+	It has to be kept as simple as possible to make the modular system clean.
+*/
 type Context struct {
 	filename string
-	repeat   bool
 	loader   loader.MediaLoader
 	renderer renderer.RenderEngine
 	size     image.Point
@@ -33,40 +36,49 @@ type Context struct {
 	options FlagOptions
 }
 
+/*
+	Creating new Context according to FlagOptions.
+*/
 func NewContext(filename string, options FlagOptions) *Context {
-	// Set default
+	// Default settings
 	ctx := Context{
 		filename: filename,
-		repeat:   options.repeat,
 		loader:   loader.LoaderMap[DEFAULT_LOADER](options.loaderoption),
 		renderer: renderer.RendererMap[DEFAULT_RENDERER](options.rendereroption),
 
 		options: options,
 	}
 
+	// Initiate Image size
 	ctx.ReloadSize()
 
+	// Custom size specified by --size
 	if s := strings.Split(options.imagesize, "x"); len(s) == 2 {
 		x, errx := strconv.Atoi(s[0])
 		y, erry := strconv.Atoi(s[1])
 		if errx == nil && erry == nil {
 			ctx.size = image.Point{x, y}
 		}
+
+		// Handle when width or height given is -1, if it is -1 this will
+		// automatically set size of other side according to the ratio of image
 		if (x == -1 || y == -1) && y != x {
 			size, err := ctx.loader.ImageSize(ctx.filename)
 			if err == nil {
 				ctx.size = internal.CalculateSizeWithRatio(ctx.size, size)
 			}
-		} else if y == x {
+		} else if y == x { // Handle when both width and height given is -1.
 			ctx.options.imagesize = ""
 			ctx.ReloadSize()
 		}
 	}
 
+	// When option for non-default loader is provided, use it
 	if createloader, exist := loader.LoaderMap[options.medialoader]; exist {
 		ctx.loader = createloader(options.loaderoption)
 	}
 
+	// When option for non-default renderer is provided, use it
 	if createrenderer, exist := renderer.RendererMap[options.renderer]; exist {
 		ctx.renderer = createrenderer(options.rendereroption)
 	}
@@ -79,6 +91,7 @@ func NewContext(filename string, options FlagOptions) *Context {
 	return accordingly unless option -f is specified.
 */
 func (c *Context) ReloadSize() {
+	// Don't need to set size according to the ratio of original image when -f flag is on
 	if !c.options.fullscreen {
 		size, err := c.loader.ImageSize(c.filename)
 		if err == nil {
@@ -102,41 +115,53 @@ func (c *Context) Load() error {
 	return nil
 }
 
+/*
+	Feeder feeds each frame to the loop itself according to specified refresh rate.
+	The pace is kept by time.Ticker. It has to send nil at the end of the stream
+	to indicate end of stream.
+*/
 func (ctx *Context) Feeder(feed chan<- image.Image, fps int) {
+	// Pace maker
 	var refresh = time.NewTicker(time.Second / time.Duration(fps))
 	defer refresh.Stop()
 
 	for i := 0; i < len(ctx.buffer); i++ {
-		if i+1 == len(ctx.buffer)-1 && ctx.repeat {
+		// If -r flag is on, loops back to the 0th frame at the end
+		if i+1 == len(ctx.buffer)-1 && ctx.options.repeat {
 			i = 0
 		}
 		select {
 		case feed <- ctx.buffer[i]:
-			<-refresh.C
-		case <-refresh.C:
+			<-refresh.C // wait for ticker
+		case <-refresh.C: // If render engine takes more time than 1/fps second, it has to skip frame
 		}
 	}
-	feed <- nil
+
+	feed <- nil // end of stream
 }
 
+/*
+	Render Loop renders frame fed from Feeder and take care of syscalls.
+*/
 func (ctx *Context) Loop() {
+
+	// Channel initiation
 	var feed = make(chan image.Image)
-	defer close(feed)
-
-	go ctx.Feeder(feed, 15)
-
 	var signals = make(chan os.Signal, 1)
 	defer close(signals)
+	defer close(feed)
 	signal.Notify(signals, os.Interrupt, syscall.SIGWINCH)
+	go ctx.Feeder(feed, 15)
 
 	for {
 		select {
+		// Syscalls
 		case sig := <-signals:
 			switch sig {
-			case os.Interrupt:
+			case os.Interrupt: // Stop at interrupt
 				return
-			case syscall.SIGWINCH:
-				if ctx.options.imagesize == "" {
+			case syscall.SIGWINCH: // Reload at Sigwinch
+				if ctx.options.imagesize == "" { // only if size is not specified
 					ctx.ReloadSize()
 					err := ctx.Load()
 					if err != nil {
@@ -146,7 +171,9 @@ func (ctx *Context) Loop() {
 				}
 			}
 			continue
+		// Frames
 		case buf := <-feed:
+			// stop at the end of stream
 			if buf == nil {
 				return
 			}
@@ -161,6 +188,9 @@ func (ctx *Context) Loop() {
 	}
 }
 
+/*
+	Stores options with FlagOptions struct
+*/
 type FlagOptions struct {
 	renderer       string
 	rendereroption string
@@ -217,6 +247,7 @@ func main() {
 		os.Exit(1)
 	}
 
+	// TTY setup
 	internal.ClearScreen()
 	internal.Cursor(false)
 	internal.SetCursorPos(0, 0)
