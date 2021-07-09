2851019ae6a1ef5eb40b9169aac59ce1ecb62b6a
SeungheonOh <dan.oh0721@gmail.com>
2021-07-08 21:34:03 -0500

    Update renderer.go

--- renderer/renderer.go
+++ renderer/renderer.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"image"
 	"time"
-	//cv "gocv.io/x/gocv"
 )
 
 var RendererMap = make(map[string]func(...string) RenderEngine)
@@ -35,20 +34,3 @@ func (r *DummyRenderer) Render(img image.Image) (string, error) {
 	time.Sleep(100 * time.Millisecond)
 	return fmt.Sprintln("Dummy Renderer", "\nImage size: ", img.Bounds()), nil
 }
-
-/*
-func Render(img cv.Mat, size image.Point, name string) error {
-	return nil
-		renderer, exist := RendererMap[name]
-		if !exist {
-			return errors.New("Renderer Not Found")
-		}
-		out, err := renderer(img, size)
-		fmt.Fprintf(os.Stdout, "\033[0;0H")
-		fmt.Fprintf(os.Stdout, out)
-		// Clear from the end of the picture to the bottom of the tty
-		// Also avoids leftover artifacts when image doesn't fill the tty
-		fmt.Fprintf(os.Stdout, "\033[J")
-		return err
-}
-*/
